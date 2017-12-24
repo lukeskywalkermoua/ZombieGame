93d89e4448860064e8dc0c1d2664f4f38734abc5
lukeskywalkermoua <33531967+lukeskywalkermoua@users.noreply.github.com>
2017-12-24 02:06:04 -0600

    Finally, I was able to successfully create a Logolauncher that shows the creator name, app title etc.., whent the user launches the app. I also learned new methods such as sleep(int time) which allows me to configure activity run-time. I'm also getting used (and finally understanding Intents, which is crucial in adroid app development). What I'm still trying to figure out is, how to properly work the AndroidManifest.xml file because I had to change my "Main" filename to the StartUpScreen Logolauncher to make it work. There has got to be a better way....

--- app/src/main/java/skywalkerapps/zombiegame/StartUpScreen.java
+++ app/src/main/java/skywalkerapps/zombiegame/StartUpScreen.java
@@ -1,11 +1,10 @@
 package skywalkerapps.zombiegame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
+import android.view.WindowManager;
+
 
 import org.w3c.dom.Text;
 
@@ -18,28 +17,45 @@ import org.w3c.dom.Text;
  */
 
 public class StartUpScreen extends AppCompatActivity {
-    private ViewGroup mContainerView;
 
-    //Method adds items from the layout that should be animated
-    private void addItemToAnimation() {
-        Animation fade_it = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.my_anim);
-        //Creat the textviews to get their ids for animation
-        TextView gameTitle = findViewById(R.id.textView);
-        gameTitle.setAnimation(fade_it);
-        TextView authorTitle = findViewById(R.id.textView2);
-        authorTitle.setAnimation(fade_it);
-        TextView companyName = findViewById(R.id.textView3);
-        companyName.setAnimation(fade_it);
-        //Adding the items to the layout view to animate
-        mContainerView.addView(gameTitle, 0);
-        mContainerView.addView(authorTitle,0);
-        mContainerView.addView(companyName,0);
-    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Gets the user's screen resolution settings to adjust for full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.startupscreen);
 
-      addItemToAnimation();
+        //Create a StartUpScreen launcher
+        LogoLauncher launchStartUp = new LogoLauncher();
 
+        //Runs code inside LogoLauncher class
+        launchStartUp.start();
+    }
+    //This class is used to Launch the StartUpScreen activity for a specific time
+    //You still need to create a LogoLauncher object in whatever class that you plan on
+    //using this code in
+    private class LogoLauncher extends Thread {
+
+        //Non-changing int for set screen display time
+        private final int DISPLAY_TIME = 4000;
+        //Uses a try catch block to try and run startupscreen for 10 seconds or 10000 millisecs
+        //Then if success, switches to a new activity screen
+        public void run() {
+            try {
+                //Method shows activity screen for param(int X) amount of milliseconds
+                sleep(DISPLAY_TIME);
+                //catch an interruption exception in case it doesn't work
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+            //Need to create an intent to change activities each time you want to switch from one to another
+            //Params, switches from this activity to the next which in this case is MainActivity
+            Intent intent = new Intent(StartUpScreen.this, MainActivity.class);
+            startActivity(intent);
+            ///.this.finish() will destroy this activity so that you can't go back to it
+            StartUpScreen.this.finish();
+
+
+        }
     }
     }
